f6510dff840d1d397c50f2cb275cda226535c99a
Shishir703 <callmebhai444@gmail.com>
2026-04-17 13:35:28 +0600

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -3,7 +3,7 @@ const mediaTypes = ["photo", 'png', "animated_image", "video", "audio"];
 
 module.exports = {
 	config: {
-		name: "Bby",
+		name: "Callad",
 		version: "1.7",
 		author: "Shishir",
 		countDown: 5,
